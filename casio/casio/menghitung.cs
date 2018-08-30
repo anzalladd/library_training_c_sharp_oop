276b28cb0631c145754f84e31545f265cc4f8e5a
anzalladd <anzalladd@gmail.com>
2018-08-30 07:24:09 +0700

    first_commit

---
+++ casio/casio/menghitung.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace casio
+{
+    public class menghitung
+    {
+        public double add(double x, double y)
+        {
+            return (x + y);
+        }
+
+        public double min(double x, double y)
+        {
+            return (x - y);
+        }
+
+        public double cross(double x, double y)
+        {
+            return (x * y);
+        }
+
+        public double devide(double x, double y)
+        {
+            return (x / y);
+        }
+    }
+}
---
+++ latihan_library/latihan_library/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using casio;
+
+namespace latihan_library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            menghitung hitung1 = new menghitung();
+            Console.Write("Masukkan x   : ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Masukkan y   : ");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("" + System.Environment.NewLine);
+            Console.Write("Hasil Tambah  = ");
+            Console.WriteLine(hitung1.add(x,y));
+            Console.Write("Hasil Kurang  = ");
+            Console.WriteLine(hitung1.min(x, y));
+            Console.Write("Hasil Kali  = ");
+            Console.WriteLine(hitung1.cross(x, y));
+            Console.Write("Hasil Bagi  = ");
+            Console.WriteLine(hitung1.devide(x, y));
+            Console.ReadLine();
+            
+        }
+    }
+}
